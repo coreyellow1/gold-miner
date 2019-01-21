5c9f380b969488148941fa118cbf19fdbacb489f
Lance Ilagan <lancemiguel@gmail.com>
2019-01-21 13:28:31 -0500

    Add Money.

--- main.js
+++ main.js
@@ -1,24 +1,33 @@
 var gameData = {
     gold: 0,
+    money: 0,
+    moneyPerGold: 3,
     goldPerClick: 1,
     goldPerClickCost: 10
   }
   
-  function mineGold() {
-    gameData.gold += gameData.goldPerClick
-    document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined"
-  }
+function mineGold() {
+  gameData.gold += gameData.goldPerClick
+  document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined"
+}
 
-  function buyGoldPerClick() {
-      if (gameData.gold >= gameData.goldPerClickCost) {
-          gameData.gold -= gameData.goldPerClickCost
-          gameData.goldPerClick += 1
-          gameData.goldPerClickCost *= 2
-          document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined"
-          document.getElementById("perClickUpgrade").innerHTML = "Upgrade Pickaxe (Currently Level " + gameData.goldPerClick + ") Cost: " + gameData.goldPerClickCost + " Gold"
-      }
-  }
+function sellGold() {
+  gameData.money += gameData.moneyPerGold * gameData.gold
+  gameData.gold = 0 
+  document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined"
+  document.getElementById("moneyTotal").innerHTML = "$ " + gameData.money
+}
 
-  var mainGameLoop = window.setInterval(function() {
-    mineGold()
-  }, 1000)
+function buyGoldPerClick() {
+    if (gameData.gold >= gameData.goldPerClickCost) {
+       gameData.gold -= gameData.goldPerClickCost
+       gameData.goldPerClick += 1
+       gameData.goldPerClickCost *= 2
+       document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined"
+       document.getElementById("perClickUpgrade").innerHTML = "Upgrade Pickaxe (Currently Level " + gameData.goldPerClick + ") Cost: " + gameData.goldPerClickCost + " Gold"
+    }
+}
+
+var mainGameLoop = window.setInterval(function() {
+  mineGold()
+}, 1000)
